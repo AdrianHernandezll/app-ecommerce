646f740407b1f7b1708c2a6a90ebad678c2a77b3
danux99 <danix08@live.com>
2021-08-27 01:54:44 -0300

    instalancion necesaria para animar las funciones

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Navbar.scss';
 import { NavLink } from 'react-router-dom';
+import $ from 'jquery';
 
 
 const NavBar = () => {
+    function animation() {
+        var tabsNewAnim = $('#navbarSupportedContent');
+        var activeItemNewAnim = tabsNewAnim.find('.active');
+        var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+        var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+        var itemPosNewAnimTop = activeItemNewAnim.position();
+        var itemPosNewAnimLeft = activeItemNewAnim.position();
+        $(".hori-selector").css({
+            "top": itemPosNewAnimTop.top + "px",
+            "left": itemPosNewAnimLeft.left + "px",
+            "height": activeWidthNewAnimHeight + "px",
+            "width": activeWidthNewAnimWidth + "px"
+        });
+        $("#navbarSupportedContent").on("click", "li", function (e) {
+            $('#navbarSupportedContent ul li').removeClass("active");
+            $(this).addClass('active');
+            var activeWidthNewAnimHeight = $(this).innerHeight();
+            var activeWidthNewAnimWidth = $(this).innerWidth();
+            var itemPosNewAnimTop = $(this).position();
+            var itemPosNewAnimLeft = $(this).position();
+            $(".hori-selector").css({
+                "top": itemPosNewAnimTop.top + "px",
+                "left": itemPosNewAnimLeft.left + "px",
+                "height": activeWidthNewAnimHeight + "px",
+                "width": activeWidthNewAnimWidth + "px"
+            });
+        });
+    }
+
+    useEffect(() => {
+
+        animation();
+        $(window).on('resize', function () {
+            setTimeout(function () { animation(); }, 500);
+        });
+
+    }, []);
+
     return (
         <nav className="navbar navbar-expand-lg navbar-mainbg">
 
@@ -14,6 +53,9 @@ const NavBar = () => {
 
             <button
                 className="navbar-toggler"
+                onClick={function () {
+                    setTimeout(function () { animation(); });
+                }}
                 type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <i className="fas fa-bars text-white"></i>
             </button>
