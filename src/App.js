edb365800f5e92478c4c07200eda9e4979caa693
danux99 <danix08@live.com>
2021-08-26 22:43:32 -0300

    Terminado la navbar e integracion de bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
+import Services from './pages/Services/Services';
+
+
 
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <Router>
+      <NavBar />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/about" exact>
+            <About />
+          </Route>
+          <Route path="/contact" exact>
+            <Contact />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/services" exact>
+            <Services />
+          </Route>
+
+          <Redirect to="/" />
+        </Switch>
+
+      </main>
+    </Router>
   );
 }
 
---
+++ src/pages/Products/Products.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Products = () => {
+    return (
+        <div className="container">
+            <h1 className="text-center" style={{ paddingTop: "30%" }}>
+                Productos
+            </h1>
+        </div>
+    )
+}
+
+export default Products
