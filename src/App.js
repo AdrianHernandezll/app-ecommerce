bf5d32c897d52286a7e1f16c7f5b08330be857fb
danux99 <danix08@live.com>
2021-09-11 18:35:06 +0200

    agregando extension jsx y arreglando count

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
-
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch
 } from 'react-router-dom';
-import ItemCount from './components/ItemCount';
 import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
 import About from './pages/About/About';
@@ -20,41 +18,32 @@ const titulo = 'MBD Store';
 
 const App = () => {
 
-  const [count, setCount] = useState(0) //seteo aca el contador
-
-  const onAdd = (cant) => {//declaro aca la funcion onadd aca para luego pasar los estados (props) a los hijos
-    setCount(cant)
-    console.log(cant);
-    console.log(count);
-  }
-
   return (
     <Router>
       <NavBar />
-      <ItemListContainer titulo={titulo} />
-      <ItemCount onAdd={onAdd} count={count} initial={1} />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/about" exact>
-            <About />
-          </Route>
-          <Route path="/contact" exact>
-            <Contact />
-          </Route>
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-          <Route path="/services" exact>
-            <Services />
-          </Route>
 
-          <Redirect to="/" />
-        </Switch>
 
-      </main>
+      <Switch>
+        <Route path="/" exact>
+          <ItemListContainer titulo={titulo} />
+          <Home />
+        </Route>
+        <Route path="/about" exact>
+          <About />
+        </Route>
+        <Route path="/contact" exact>
+          <Contact />
+        </Route>
+        <Route path="/products" exact>
+          <Products />
+        </Route>
+        <Route path="/services" exact>
+          <Services />
+        </Route>
+
+        <Redirect to="/" />
+      </Switch>
+
     </Router>
   );
 }
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import ItemCount from './ItemCount';
+
+const ItemListContainer = ({ titulo }) => {
+    const [count, setCount] = useState(0) //seteo aca el contador
+
+    const onAdd = (cant) => {//declaro aca la funcion onadd aca para luego pasar los estados (props) a los hijos
+        setCount(cant)
+        console.log(cant);
+        console.log(count);
+    }
+    return (
+        <>
+            <p className="text-center ">Hola bienvenido al {titulo} </p>
+            <ItemCount onAdd={onAdd} stock={8} initial={1} />
+        </>
+    )
+}
+
+export default ItemListContainer
